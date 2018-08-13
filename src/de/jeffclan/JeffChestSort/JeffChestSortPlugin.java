b21b12f6a59efcfaf584680ed33eec33af5d545c
mfnalex <1122571+mfnalex@users.noreply.github.com>
2018-08-13 02:57:54 +0200

    Added hardcoded default values for config to avoid Exceptions

--- src/de/jeffclan/JeffChestSort/JeffChestSortPlugin.java
+++ src/de/jeffclan/JeffChestSort/JeffChestSortPlugin.java
@@ -42,6 +42,16 @@ public class JeffChestSortPlugin extends JavaPlugin {
 		File playerDataFolder = new File(getDataFolder().getPath() + File.separator + "playerdata");
 		if (!playerDataFolder.exists())
 			playerDataFolder.mkdir();
+		
+		getConfig().addDefault("sorting-enabled-by-default", false);
+		getConfig().addDefault("show-message-when-using-chest", true);
+		getConfig().addDefault("show-message-when-using-chest-and-sorting-is-enabled", false);
+		getConfig().addDefault("show-message-again-after-logout", true);
+		getConfig().addDefault("message-when-using-chest","&7Hint: Type &6/chestsort&7 to enable automatic chest sorting.");
+		getConfig().addDefault("message-when-using-chest2","&7Hint: Type &6/chestsort&7 to disable automatic chest sorting.");
+		getConfig().addDefault("message-sorting-disabled","&7Automatic chest sorting has been &cdisabled&7.&r");
+		getConfig().addDefault("message-sorting-enabled","&7Automatic chest sorting has been &aenabled&7.&r");
+		getConfig().addDefault("message-error-players-only","&cError: This command can only be run by players.&r");
 	}
 	
 	String getSortableString(ItemStack item) {
