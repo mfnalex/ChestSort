1d85716207f7a382995c5908a19768e77efb5ed8
mfnalex <1122571+mfnalex@users.noreply.github.com>
2018-08-13 13:44:08 +0200

    Moved default messages from main class to Messages class

--- src/de/jeffclan/JeffChestSort/JeffChestSortPlugin.java
+++ src/de/jeffclan/JeffChestSort/JeffChestSortPlugin.java
@@ -49,11 +49,6 @@ public class JeffChestSortPlugin extends JavaPlugin {
 		getConfig().addDefault("show-message-when-using-chest", true);
 		getConfig().addDefault("show-message-when-using-chest-and-sorting-is-enabled", false);
 		getConfig().addDefault("show-message-again-after-logout", true);
-		getConfig().addDefault("message-when-using-chest","&7Hint: Type &6/chestsort&7 to enable automatic chest sorting.");
-		getConfig().addDefault("message-when-using-chest2","&7Hint: Type &6/chestsort&7 to disable automatic chest sorting.");
-		getConfig().addDefault("message-sorting-disabled","&7Automatic chest sorting has been &cdisabled&7.&r");
-		getConfig().addDefault("message-sorting-enabled","&7Automatic chest sorting has been &aenabled&7.&r");
-		getConfig().addDefault("message-error-players-only","&cError: This command can only be run by players.&r");
 	}
 	
 	void unregisterPlayer(Player p) {
