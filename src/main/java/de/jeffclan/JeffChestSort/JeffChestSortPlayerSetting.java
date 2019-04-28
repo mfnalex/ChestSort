ea5acd924357b82f3692ae9850bf2356cc99b3a0
JEFF <1122571+mfnalex@users.noreply.github.com>
2019-04-29 01:27:48 +0200

    Update JeffChestSortPlayerSetting.java

--- src/main/java/de/jeffclan/JeffChestSort/JeffChestSortPlayerSetting.java
+++ src/main/java/de/jeffclan/JeffChestSort/JeffChestSortPlayerSetting.java
@@ -1,6 +1,11 @@
 package de.jeffclan.JeffChestSort;
 
 public class JeffChestSortPlayerSetting {
+	
+	// Represents the information regarding a player
+	// That includes:
+	// - Does this player has sorting enabled?
+	// - Did this player see the message on how to use ChestSort (message-when-using-chest in config.yml)
 
 	// Sorting enabled for this player?
 	boolean sortingEnabled;
