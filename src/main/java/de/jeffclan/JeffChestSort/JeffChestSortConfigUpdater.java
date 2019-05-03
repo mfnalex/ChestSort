a0828890d8adeaf2313551e23c263a6860cbbfe7
mfnalex <1122571+mfnalex@users.noreply.github.com>
2019-05-03 12:35:15 +0200

    ConfigUpdater

---
+++ src/main/java/de/jeffclan/JeffChestSort/JeffChestSortConfigUpdater.java
@@ -0,0 +1,10 @@
+package de.jeffclan.JeffChestSort;
+
+public class JeffChestSortConfigUpdater {
+
+	// Admins hate config updates. Just relax and let ChestSort update to the newest config version
+	// Don't worry! Your changes will be kept
+	
+	
+
+}
