7edb355118f4fd8ae1ae4b788e3f4e7fc8b2c8b1
Resi <unconfigured@null.spigotmc.org>
2022-09-24 13:49:27 +0200

    Add Hopper to Hotkeys

--- src/main/java/de/jeff_media/chestsort/listeners/ChestSortListener.java
+++ src/main/java/de/jeff_media/chestsort/listeners/ChestSortListener.java
@@ -656,6 +656,7 @@ public class ChestSortListener implements org.bukkit.event.Listener {
                 && e.getInventory().getType() != InventoryType.DISPENSER
                 && e.getInventory().getType() != InventoryType.DROPPER
                 && e.getInventory().getType() != InventoryType.ENDER_CHEST
+                && e.getInventory().getType() != InventoryType.HOPPER
                 && !e.getInventory().getType().name().equalsIgnoreCase("SHULKER_BOX")
                 && (e.getInventory().getHolder() == null || !e.getInventory().getHolder().getClass().toString().endsWith(".CraftBarrel"))
                 && e.getInventory() != p.getEnderChest()
